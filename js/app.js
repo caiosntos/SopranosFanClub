509b29ff0a2dd959677c44368682c523701cbaed
caiosntos <caiogiovanists@outlook.com>
2025-03-19 21:53:21 -0300

    main

---
+++ js/app.js
@@ -0,0 +1,45 @@
+function validaFaleConosco(){
+    if(document.frmfaleconosco.txtnome.value==""){
+        alert("Preencha o campo Nome.");
+        document.frmfaleconosco.textnome.focus();
+        return false;
+    }
+
+    if(document.frmfaleconosco.txtfone.value==""){
+        alert("Preencha o seu número de telefone");
+        document.frmfaleconosco.txtfone.focus();
+        return false;
+    }
+
+    if(document.frmfaleconosco.txtemail.value==""){
+        alert("Preencha o seu e-mail");
+        document.frmfaleconosco.txtemail.focus();
+        return false;
+    }
+
+    if(document.frmfaleconosco.selmotivo.value==""){
+        alert("Preencha o motivo");
+        document.frmfaleconosco.selmotivo.focus();
+        return false;
+    }
+
+    if(document.frmfaleconosco.txcomentario.value==""){
+        alert("Preencha o comentário");
+        document.frmfaleconosco.txcomentario.focus();
+        return false;
+    }
+
+
+    return true;
+}
+
+function enable() {
+    var butao = document.getElementById('butao');
+    var submit = document.getElementsByName('submit')[0]; 
+
+    if (butao.checked) {
+      submit.removeAttribute("disabled"); 
+    } else {
+      submit.setAttribute("disabled", "true");
+    }
+  }
