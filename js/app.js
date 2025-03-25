cfdf9b600a7fe14e25f175ca9bc81f403e981c9b
caiosntos <caiogiovanists@outlook.com>
2025-03-25 20:55:18 -0300

    feat:Implantando a validação do nome, número de telefone e email. Também colocando placeholders nos inputs de textos.

--- js/app.js
+++ js/app.js
@@ -1,34 +1,27 @@
 function validaFaleConosco(){
-    if(document.frmfaleconosco.txtnome.value==""){
-        alert("Preencha o campo Nome.");
-        document.frmfaleconosco.textnome.focus();
+    var nome = document.frmfaleconosco.txtnome.value;
+    var expRegNome = new RegExp ("^[A-Za-zÀ-ü]{3,}( [A-Za-zÀ-ü]+)+$");
+    if(!expRegNome.test(nome)){
+        alert("Preencha o campo Nome corretamente.");
+        document.frmfaleconosco.txtnome.focus();
         return false;
     }
 
-    if(document.frmfaleconosco.txtfone.value==""){
-        alert("Preencha o seu número de telefone");
+    var fone = document.frmfaleconosco.txtfone.value;
+    var exRegFone = RegExp("^[(]{1}[1-9]{2}[)]{1}[0-9]{4,5}[-]{1}[0-9]{4}$");
+    if(!exRegFone.test(fone)){
+        alert("Preencha o seu número de Telefone corretamente.");
         document.frmfaleconosco.txtfone.focus();
         return false;
     }
 
-    if(document.frmfaleconosco.txtemail.value==""){
-        alert("Preencha o seu e-mail");
+    var email = document.frmfaleconosco.txtemail.value;
+    var exRegMail = new RegExp("\\S+@\\S+\\.\\S+");
+    if(!exRegMail.test(email)){
+        alert("Preencha o seu E-mail corretamente.");
         document.frmfaleconosco.txtemail.focus();
         return false;
     }
-
-    if(document.frmfaleconosco.selmotivo.value==""){
-        alert("Preencha o motivo");
-        document.frmfaleconosco.selmotivo.focus();
-        return false;
-    }
-
-    if(document.frmfaleconosco.txcomentario.value==""){
-        alert("Preencha o comentário");
-        document.frmfaleconosco.txcomentario.focus();
-        return false;
-    }
-
 
     return true;
 }
@@ -43,3 +36,5 @@ function enable() {
         submit.setAttribute("disabled", "true");
     }
   }
+
+  
